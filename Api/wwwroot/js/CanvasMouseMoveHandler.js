42e4b26bf9911899e294ed42a0b62d31871c07c2
Andrew Boudreau <andrewboudreau@gmail.com>
2023-01-06 22:17:55 -0600

    update index, more sprite manager work

--- Api/wwwroot/js/CanvasMouseMoveHandler.js
+++ Api/wwwroot/js/CanvasMouseMoveHandler.js
@@ -3,7 +3,21 @@
     // Get the bounding client rect of the canvas element
     const rect = canvas.getBoundingClientRect();
 
-    const mouse = { x: 0, y: 0 };
+    const draw = ctx => {
+        ctx.font = '12px sans-serif';
+        ctx.textAlign = 'center';
+        ctx.fillStyle = 'white';
+        ctx.fillText(`(${Math.round(mouse.x)}, ${Math.round(mouse.y)})`, mouse.x, mouse.y);
+
+        ctx.fillStyle = "#000000";
+        ctx.fillRect(mouse.x, mouse.y, 4, 4);
+    }
+
+    const mouse = {
+        x: 0,
+        y: 0,
+        draw: draw
+    };
 
     canvas.addEventListener('mousemove', (event) => {
         // Calculate the mouse position relative to the canvas
